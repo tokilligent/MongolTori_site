7d29dcd8da84074afc3312a31c59fe3755f893ba
Sadikul Alim Toki <53906445+tokilligent@users.noreply.github.com>
2021-07-16 02:10:35 +0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -110,8 +110,8 @@
 			$from = $_POST['email']; // this is the sender's Email address
 			$name = $_POST['name'];
 			$subject = $_POST['subject'];
-			$message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-			$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+			$message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+			$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
 			$headers = "From:" . $from;
 			//$headers2 = "From:" . $to;
